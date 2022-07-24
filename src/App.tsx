b0b3257b34de5261ca9bbd1ec47190cd8d62353d
Marko Solopenko <marko.solopenko@yalantis.net>
2022-07-24 20:23:55 +0300

    deleted meta

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Games from "./pages/Games/Games";
 
 function App() {
+  useEffect(() => {
+    const meta = document.createElement("meta");
+    meta.httpEquiv = "Content-Security-Policy";
+    meta.content = "upgrade-insecure-requests";
+    document.head.append(meta);
+  }, []);
+
   return (
     <div className="App">
       <Games />
