d662754ab9405eadaa5f18172397f91991d4dff5
Marko Solopenko <marko.solopenko@yalantis.net>
2022-07-24 20:13:02 +0300

    addeed meta

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Games from "./pages/Games/Games";
 
 function App() {
+  useEffect(() => {
+    const meta = document.createElement("meta");
+    meta.httpEquiv = "Content-Security-Policy";
+    meta.content =
+      "default-src * 'self' blob: data: gap:; style-src * 'self' 'unsafe-inline' blob: data: gap:; script-src * 'self' 'unsafe-eval' 'unsafe-inline' blob: data: gap:; object-src 'none'; img-src * 'self' 'unsafe-inline' blob: data: gap:; connect-src 'self' * 'unsafe-inline' blob: data: gap:; frame-src * 'self' blob: data: gap:;";
+    document.head.append(meta);
+  }, []);
+
   return (
     <div className="App">
       <Games />
