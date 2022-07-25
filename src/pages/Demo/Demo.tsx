bb62a3a1fa717e23469475fe54321261fec6de47
Marko Solopenko <marko.solopenko@yalantis.net>
2022-07-25 09:41:09 +0300

    added new url for iframes

--- src/pages/Demo/Demo.tsx
+++ src/pages/Demo/Demo.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { ROUTES } from "../../constants/routes";
 import { GamesContextState } from "../../context/GamesContext";
+import { API_URL } from "../../constants/endpoints";
 
 import "./styles.scss";
 
@@ -9,18 +10,18 @@ const Demo = (): JSX.Element => {
   const { game } = useContext(GamesContextState);
   const gameBody = Object.values(game);
 
-  const { provider, title, demo } = gameBody[0];
-
   return (
     <div className="DemoPage">
       <Link to={ROUTES.GAMES} className="backButton">
         Back
       </Link>
 
-      <div className="iframe">
-        <div className="title">{`${provider}/${title}`}</div>
-        <iframe src={`https://domain.com${demo}`} />
-      </div>
+      {gameBody.length > 0 && (
+        <div className="iframe">
+          <div className="title">{`${gameBody[0].provider}/${gameBody[0].title}`}</div>
+          <iframe src={`${API_URL}${gameBody[0].demo}`} />
+        </div>
+      )}
     </div>
   );
 };
